c39425f413a760ba500a4b39cfc2b64bb09b76dc
Arman Emamhosseini <75748147+ThatGuy247@users.noreply.github.com>
2024-05-19 13:24:36 +0330

    Update SloganCounter.sol

--- SloganCounter.sol
+++ SloganCounter.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
- // fuck gitcoin -
+ // fuck gitcoin - again
 contract SloganCounter {
     mapping(string => uint256) private slogans;
     mapping(uint256 => string) private topSlogans;
